da1d6727b013f95a22be9d88fb75bb16ba2ece24
Vishvak03 <vishvak.logar_asp25@ashoka.edu.in>
2024-09-29 00:32:54 +0530

    Update a1-5.cpp

--- a1-5.cpp
+++ a1-5.cpp
@@ -23,12 +23,24 @@ class Stack {
 
         // Function to add an element to the stack
         void push(int x) {
-            //Write your code here
+            if (top== capacity-1){
+                cout << "Stack Overflow\n";
+                return;
+            }
+            top += 1;
+            arr[top]= x;
         }
 
         // Function to pop the top element
         int pop() {
-            //Write your code here
+            if (top== -1) {
+                cout << "Stack Underflow\n";
+                return -1;
+            }
+            int  x= arr[top];
+            arr[top]= -1;
+            top -=1;
+            return x;
         }
 
         // Function to display the elements of the stack
@@ -47,15 +59,17 @@ class Stack {
     
         // Function to check if the stack is full
         bool isFull() const {
-            //Write your code here
+            return top== capacity-1;
         }
 
         // Function to check if the stack is empty
         bool isEmpty() const {
-            //Write your code here
+            return top==-1;
         }
+        
 
 };
+   
 
 // Stacks have been defined globally for the towers A, B, and C for printing the towers at each step
 Stack* A;
@@ -74,13 +88,40 @@ void displayTowers() {
 }
 
 void rearrangeDisks(int n, Stack& A, Stack& B, Stack& C, char from, char to, char aux) {
-    //Write your code here
+
+    if (n==0) {
+        return;
+    }
+    if (n==1) {
+        int disk;  
+        disk = A.pop();
+        C.push(disk);
+        cout << "Move disk " << disk << " from " << from << " to " << to << endl;
+        displayTowers();
+        return;
+    }
+    rearrangeDisks(n-1,A, C, B, 'A', 'B', 'C');
+
+    int disk;  
+    disk = A.pop();
+    C.push(disk);
+    cout << "Move disk " << disk << " from " << from << " to " << to << endl;
+    displayTowers();
+
+    rearrangeDisks(n-1,B, A, C, 'B', 'C', 'A');
+
+
 }
 
 int main() { // The main function has been defined for you, do not edit anything here.
     int n;
     cout << "Enter the number of disks: ";
     cin >> n;
+    
+    if ((cin.fail()) or (n<0)) {
+        cout << "Invalid Input ";
+        return -1;
+    }    
 
     A = new Stack(n);
     B = new Stack(n);
@@ -89,7 +130,7 @@ int main() { // The main function has been defined for you, do not edit anything
     for (int i = n; i >= 1; i--) {
         A->push(i);
     }
-
+ 
     displayTowers();
 
     rearrangeDisks(n, *A, *B, *C, 'A', 'C', 'B');
@@ -100,3 +141,4 @@ int main() { // The main function has been defined for you, do not edit anything
 
     return 0;
 }
+
